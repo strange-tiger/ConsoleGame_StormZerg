7b33944002da8a4ae74dfbe6adc069d501acd82d
strange-tiger <jinolee9525@gmail.com>
2022-05-13 11:33:08 +0900

    22 05 14 commit #4
    Refactor: * 프레임 한번한번마다 생각을 쪼갤 것.
    수정의 요지 - 유지 보수, 가독성
    1. 프레임 단위로 생각을 나누어서 구현할 것. -> system("cls")를 줄이는 등.
    2. 조건문에 &&등으로 추가 조건을 다는 것은 가독성이 좋지 못하다. -> 이중 if 처리
    3. 입력, 처리, 출력을 나눌 것. -> bullet bool 변수 활용
    4. 전역변수는 꼭 필요할 때만. -> 구조체 위치 변경
    5. bullet.shot은 bullet이 쏘여진 상태라는 의미를 알기에는 어렵다. → bullet.isFired
    6. 유지 보수를 생각하고 코드를 짤 것. -> struct Player player 선언

--- FrameWorkPractice/PBullet/PBullet/Main.c
+++ FrameWorkPractice/PBullet/PBullet/Main.c
@@ -4,88 +4,108 @@
 #include <time.h>
 
 #define GetKey(vkey) 0x8000 & GetAsyncKeyState(vkey)
-#define SPACE_MOVING 1
+#define PLAYER_MOVE_SPEED 1
+#define BULLET_MOVE_SPEED 1
 #define BOUNDARY_X 32
 #define BOUNDARY_Y 16
-#define BULLET_DURATION 3.0f
+#define PLAYER_INIT_POS_X 16
+#define PLAYER_INIT_POS_Y 8
 
-struct Bullet
-{
-	COORD pos;
-	float life;
-	bool shot;
-} bullet;
+#define BULLET_DURATION 3.0f
 
 int main(void)
 {
-	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-	clock_t prevTick = clock();
-	COORD playerPos = { 16, 8 };
+	struct Bullet
+	{
+		COORD pos;
+		float life;
+		bool isFired; // isShot, isShotten, isShooting, exist, shootOut, fired, isFired, flying
+	} bullet;
 	bullet.pos.X = 0;
 	bullet.pos.Y = 0;
 	bullet.life = 0.0f;
-	bullet.shot = 0;
+	bullet.isFired = 0;
 
-	SetConsoleCursorPosition(consoleHandle, playerPos);
-	printf("P");
+	struct Player
+	{
+		COORD pos;
+	} player;
+	player.pos.X = PLAYER_INIT_POS_X;
+	player.pos.Y = PLAYER_INIT_POS_Y;
+
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	clock_t prevTick = clock();
+	
 	while (1)
 	{
-		// 입력 // 처리
-		// 화살표 키가 눌리면 SPACE_MOVING만큼 이동하도록
-		if (GetKey(VK_LEFT) && playerPos.X > 0)
-		{
-			playerPos.X -= SPACE_MOVING;
-		}
-		if (GetKey(VK_RIGHT) && playerPos.X < BOUNDARY_X)
-		{
-			playerPos.X += SPACE_MOVING;
-		}
-		if (GetKey(VK_UP) && playerPos.Y > 0)
-		{
-			playerPos.Y -= SPACE_MOVING;
-		}
-		if (GetKey(VK_DOWN) && playerPos.Y < BOUNDARY_Y)
-		{
-			playerPos.Y += SPACE_MOVING;
-		}
-
-		if (GetKey(VK_SPACE))
-		{
-			bullet.pos.X = playerPos.X + 1;
-			bullet.pos.Y = playerPos.Y;
-			bullet.life = BULLET_DURATION;
-
-			bullet.shot = true;
-		}
-		
-		// 출력
-		system("cls");
-		SetConsoleCursorPosition(consoleHandle, playerPos);
-		printf("P");
-
 		// 델타 타임
 		clock_t currentTick = clock();
 		float deltaTime = (float)(currentTick - prevTick) / CLOCKS_PER_SEC;
 		prevTick = currentTick;
 
-		if (bullet.shot && bullet.life > 0.0f)
+		// 입력 // 처리
+		// 화살표 키가 눌리면 MOVE_SPEED만큼 이동하도록
+		if (GetKey(VK_LEFT))
+		{
+			if (player.pos.X > 0)
+			{
+				player.pos.X -= PLAYER_MOVE_SPEED;
+			}
+		}
+		if (GetKey(VK_RIGHT))
+		{
+			if (player.pos.X < BOUNDARY_X)
+			{
+				player.pos.X += PLAYER_MOVE_SPEED;
+			}
+		}
+		if (GetKey(VK_UP))
+		{
+			if (player.pos.Y > 0)
+			{
+				player.pos.Y -= PLAYER_MOVE_SPEED;
+			}
+		}
+		if (GetKey(VK_DOWN))
+		{
+			if (player.pos.Y < BOUNDARY_Y)
+			{
+				player.pos.Y += PLAYER_MOVE_SPEED;
+			}
+		}
+
+		if (GetKey(VK_SPACE))
+		{
+			bullet.pos.X = player.pos.X + 1;
+			bullet.pos.Y = player.pos.Y;
+			bullet.life = BULLET_DURATION;
+
+			bullet.isFired = true;
+		}
+
+		// 처리
+		if (bullet.life > 0.0f)
 		{
 			bullet.life -= deltaTime;
+			bullet.pos.X += BULLET_MOVE_SPEED;
+		}
+		else
+		{
+			bullet.isFired = false;
+			bullet.life = 0.0f;
+		}
 
-			bullet.pos.X += SPACE_MOVING;
-
+		// 출력
+		system("cls");
+		SetConsoleCursorPosition(consoleHandle, player.pos);
+		printf("P");
+		if (bullet.isFired)
+		{
 			SetConsoleCursorPosition(consoleHandle, bullet.pos);
 			printf("O");
 		}
-		else if (bullet.life < 0.0f)
-		{
-			bullet.shot = false;
-			bullet.life = 0.0f;
 
-			system("cls");
-		}
-
-		Sleep(20);
+		Sleep(10);
 	}
 	return 0;
 }
